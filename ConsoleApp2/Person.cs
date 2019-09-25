d4080aceb43a79ab0014ce618554a5d91d6a5512
roberthuul <50729144+roberthuul@users.noreply.github.com>
2019-09-25 11:43:33 +0300

    Add files via upload

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ShoppingCart ShoppingCart { get; set; }
+
+        public Color FavouriteColor { get; set; }
+
+        public Person(string eesnimi, string perenimi)
+        {
+            FirstName = eesnimi;
+            LastName = perenimi;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    public enum Color
+    {
+        Teadmata,
+        Punane,
+        Oranž,
+        Kollane,
+        Roheline,
+        Sinine,
+        Lilla,
+        Valge,
+        Must
+    }
+}
---
+++ FavouriteColor/Program.cs
@@ -0,0 +1,50 @@
+﻿using ConsoleApp2;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            //string[] lines = File.ReadAllLines(@"C:\Users\opilane\Desktop\list.txt");
+            string[] lines = File.ReadAllLines("list.txt");
+
+            foreach (var item in lines)
+            {
+                var names = item.Split(' ');
+                Person person = new Person(names[0], names[1]);
+                people.Add(person);
+            }
+            Console.WriteLine("Sisesta oma perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            //foreach (var item in people)
+            //{
+            //    if (lastName == item.LastName)
+            //    {
+            //        Console.WriteLine("Tere, " + item.FirstName);
+            //    }
+            //}
+
+            Person someone = people.FirstOrDefault(x => x.LastName == lastName);
+            Console.WriteLine($"Tere, {someone.FirstName}! Mis on sinu lemmikvärv?");
+            var answer = Console.ReadLine();
+
+            Enum.TryParse(answer, true, out Color favouriteColor);
+            someone.FavouriteColor = favouriteColor;
+
+            using (StreamWriter file = new StreamWriter("list2.txt"))
+            {
+                file.WriteLine($"{someone.FirstName} {someone.LastName}, lemmikvärv: {someone.FavouriteColor}");
+            }
+            //Console.ReadKey();
+        }
+    }
+}
